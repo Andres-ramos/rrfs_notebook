c05de05cb6834c5535a3a229b082d242bbc24e87
Andres-ramos <ramosandres443@gmail.com>
2023-07-26 00:34:36 -0400

    refactored file names

--- run_threshold.py
+++ run_threshold.py
@@ -7,7 +7,7 @@ end_date = pd.Timestamp(2023,6,30)
 
 for date in pd.date_range(start=start_day, end=end_date, freq="D"):
     day = date.strftime("%Y-%m-%d")
-    result = subprocess.run(["sbatch", "threshold_script.sh", day], capture_output=True)
+    result = subprocess.run(["sbatch", "threshold_script.sh", day], capture_output=True, text=True)
     print(result)
 
 

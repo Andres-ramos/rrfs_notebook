6c3b075999b968564581dc6197983355f3fe42b6
Andres-ramos <ramosandres443@gmail.com>
2023-07-27 14:04:34 -0400

    Pipeline updates.

--- run_pipeline.py
+++ run_pipeline.py
@@ -7,7 +7,10 @@ end_date = pd.Timestamp(2023,6,30)
 
 for date in pd.date_range(start=start_day, end=end_date, freq="D"):
     day = date.strftime("%Y-%m-%d")
-    result = subprocess.run(["sbatch", "day_script.sh", day], capture_output=True, text=True)
+    result = subprocess.run(["sbatch", "day_script.sh", day, 3, "3t1"], capture_output=True, text=True)
+    result = subprocess.run(["sbatch", "day_script.sh", day, 3, "9t1"], capture_output=True, text=True)
+    result = subprocess.run(["sbatch", "day_script.sh", day, 15, "3t1"], capture_output=True, text=True)
+    result = subprocess.run(["sbatch", "day_script.sh", day, 15, "9t1"], capture_output=True, text=True)
 
 
 
